2abaea24a27610de81d881927ee100b09b6a115b
Michael Anderson <mganderson87@gmail.com>
2017-04-26 08:53:04 -0400

    Revised user, webapp_user models '

--- app/models/webapp_user.py
+++ app/models/webapp_user.py
@@ -1,9 +1,9 @@
 from ferris import BasicModel, Model
 from google.appengine.ext import ndb
 
-class User(Model):
+class WebappUser(Model):
     unique_id = ndb.StringProperty(required=True, indexed=True) # TODO: Make this the key, don't autogen key
     email = ndb.StringProperty(indexed=True)
-    # hashed_password = ndb.IntegerProperty()
+    hashed_password = ndb.IntegerProperty()
     origin_station = ndb.StringProperty(required=True)
     destination_station = ndb.StringProperty(required=True)
